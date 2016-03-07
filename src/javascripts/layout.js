2197a6665d69321145e85e355cd99bb00808b1ee
Kim <331599158@qq.com>
2016-03-07 18:13:49 +0800

    增加awk.sh。

--- src/javascripts/layout.js
+++ src/javascripts/layout.js
@@ -20,6 +20,7 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when('/df', { templateUrl: 'templates/df.html' })
     .when('/perm777', { templateUrl: 'templates/perm777.html' })
     .when('/apache', { templateUrl: 'templates/apache.html'})
+    .when('/awk', { templateUrl: 'templates/awk.html' })
     .otherwise({ templateUrl: 'templates/df.html' });
   }
 ]);
---
+++ src/shellscripts/awk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WEB_FILE=../../public/templates/awk.html
+rm -f $WEB_FILE
+
+echo "<hr><h1>awk 版本信息</h1><p><code>awk -W version</code></p>" >> $WEB_FILE
+echo "<pre>$(awk -W version)</pre>" >> $WEB_FILE
